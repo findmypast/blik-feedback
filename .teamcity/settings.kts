57df005932b8a4e5766f7dbd255b67cc5c90d5ac
Jazzmin-Washington <jazzmin.s.washington@gmail.com>
2026-08-04 13:08:11 +0100

    OD-1179: Adding teamcity connections to blik-feedback so it can trigger blik Docker build

---
+++ .teamcity/settings.kts
@@ -0,0 +1,52 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+version = "2024.03"
+
+project {
+    buildType(BuildScanTestTagWithVersion)
+
+    params {
+        param("git.projectname", "blik-feedback")
+    }
+
+    subProject(tech_docs)
+}
+
+object BuildScanTestTagWithVersion : BuildType({
+    templates(AbsoluteId("BuildScanTestTagWithVersionMain"))
+    name = "Build, scan, test & tag with version"
+
+    params {
+        param("git.projectname", "blik-feedback")
+        param("enable_vulnerability_scanning", "true")
+        param("disable_npm_audit", "true")
+    }
+})
+
+object tech_docs : Project({
+    name = "Publish Backstage Techdocs"
+    description = "Build and generate docs for consumption within Backstage"
+
+    buildType(PublishBackstageTechdocs_PublishTechDocs)
+})
+
+object PublishBackstageTechdocs_PublishTechDocs : BuildType({
+    templates(AbsoluteId("PublishBackstageTechDocsMain"))
+    name = "Publish TechDocs"
+    description = "Build and publish tech docs"
+
+    params {
+        param("backstage_techdocs_component_name", "blik-feedback")
+    }
+
+    triggers {
+        vcs {
+            id = "TRIGGER_BLIK_FEEDBACK_TECHDOCS"
+            triggerRules = """
+                +:docs/**
+                +:mkdocs.yml
+            """.trimIndent()
+        }
+    }
+})
