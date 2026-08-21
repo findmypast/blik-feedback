776abf4dd7329db5bc2fee96a041e7fa1cb45640
Jazzmin-Washington <jazzmin.s.washington@gmail.com>
2026-08-21 17:03:12 +0100

    OD-1183: Route error pages back to the application

--- blik/test_error_handlers.py
+++ blik/test_error_handlers.py
@@ -1,7 +1,9 @@
 from django.test import TestCase, override_settings
+from django.test import RequestFactory
 from django.urls import reverse
 
 from accounts.factories import UserProfileFactory
+from blik.views import handler500
 
 
 @override_settings(DEBUG=False)
@@ -28,3 +30,15 @@ class ErrorHandlerTests(TestCase):
             f'{reverse("login")}?next=/missing-page/',
             fetch_redirect_response=False,
         )
+
+    def test_500_page_only_offers_login_and_retry_actions(self):
+        response = handler500(RequestFactory().get('/broken/'))
+        content = response.content.decode()
+
+        self.assertEqual(response.status_code, 500)
+        self.assertIn(f'href="{reverse("login")}"', content)
+        self.assertIn('Back to Login', content)
+        self.assertIn('Try Again', content)
+        self.assertNotIn('Back to Home', content)
+        self.assertNotIn('>Home</a>', content)
+        self.assertNotIn('GitHub', content)
